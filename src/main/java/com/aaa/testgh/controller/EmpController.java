28a99f088513eaf84c80197e32a399d4121ecf01
aaaaaahpy <863477636@qq.com>
2018-11-29 10:51:22 +0800

    xhgd

--- src/main/java/com/aaa/testgh/controller/EmpController.java
+++ src/main/java/com/aaa/testgh/controller/EmpController.java
@@ -7,4 +7,7 @@ package com.aaa.testgh.controller;
  * createTime:2018-11-29 09:37
  */
 public class EmpController {
+    public static void main(String[] args) {
+        System.out.println("测试合并分支1111111111111");
+    }
 }
